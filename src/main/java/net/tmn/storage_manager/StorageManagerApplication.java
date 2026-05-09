2609998ce385dc2146f570dd0d9dbdf1af4a7a36
TonnyMunkNielsen <TonnyMunkNielsen@protonmail.ch>
2026-05-09 22:15:55 +0200

    Spotless.

--- src/main/java/net/tmn/storage_manager/StorageManagerApplication.java
+++ src/main/java/net/tmn/storage_manager/StorageManagerApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class StorageManagerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(StorageManagerApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(StorageManagerApplication.class, args);
+    }
 }
