e2927b7fdffc8d2fab84c63fcbc976283535e2b7
TonnyMunkNielsen <TonnyMunkNielsen@protonmail.ch>
2026-05-09 22:09:19 +0200

    Initial commit of Spring Boot 4 + Vaadin project.

---
+++ src/test/java/net/tmn/storage_manager/StorageManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package net.tmn.storage_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StorageManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
